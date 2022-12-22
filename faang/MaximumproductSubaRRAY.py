912890b42f239060bd5e566349fd751f1c4b2eef
klaus19 <tejaskhartude@gmail.com>
2022-12-22 16:06:30 +0530

    updated

---
+++ faang/MaximumproductSubaRRAY.py
@@ -0,0 +1,25 @@
+class Solution(object):
+
+    def max_product_subarray(self, nums):
+        if len(nums) == 0:
+            return 0
+
+        max_so_far = nums[0]
+        min_so_far = nums[0]
+        result = max_so_far
+
+        for i in range(1, len(nums)):
+            curr = nums[i]
+            temp_max = max(curr, max_so_far * curr, min_so_far * curr)
+            min_so_far = min(curr, max_so_far * curr, min_so_far * curr)
+
+            max_so_far = temp_max
+
+            result = max(max_so_far, result)
+
+        return result
+
+
+if __name__ == "__main__":
+    ltr = [2, 3, -2, 4]
+    print(Solution().max_product_subarray(ltr))
