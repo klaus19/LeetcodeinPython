b231b2715f9cc35e2fef0ead218e23337bb20487
klaus19 <tejaskhartude@gmail.com>
2023-01-23 21:52:43 +0530

    added a new problem a Blind 75 problem

---
+++ Blind75/BestTimeBuySellStock.py
@@ -0,0 +1,17 @@
+class Solution(object):
+
+    def  buySell(self,prices):
+
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            min_price = min(min_price, price)
+            max_profit = max(max_profit, price-min_price)
+        return max_profit
+
+if __name__ == '__main__':
+    st = [7,1,5,3,6,4]
+    pt = [7,6,4,3,1]
+    print(Solution().buySell(st))
+    print(Solution().buySell(pt))
