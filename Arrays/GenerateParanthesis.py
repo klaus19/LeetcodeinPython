eeec550ce5d84e90f364de7f4985c959502013b8
klaus19 <tejaskhartude@gmail.com>
2023-01-23 19:58:44 +0530

    added a new problem

---
+++ Arrays/GenerateParanthesis.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution(object):
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        def helper(ans, s, left, right):
+            if left == 0 and right == 0:
+                ans.append(s)
+
+            if left > 0:
+                helper(ans, s + '(', left - 1, right)
+
+            if right > 0 and left < right:
+                helper(ans, s + ')', left, right - 1)
+
+        ans = []
+        helper(ans, '', n, n)
+
+        return ans
+
+if __name__ == '__main__':
+    n =3
+    print(Solution().generateParenthesis(n))
