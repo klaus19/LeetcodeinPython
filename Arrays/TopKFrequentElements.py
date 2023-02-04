880d256b0f037c6b3a750bd8d18231f29dbb02a7
klaus19 <tejaskhartude@gmail.com>
2023-02-04 19:27:55 +0530

    made some changes in the code

--- Arrays/TopKFrequentElements.py
+++ Arrays/TopKFrequentElements.py
@@ -8,3 +8,8 @@ class Solution(object):
         count = Counter(nums)
         return heapq.nlargest(k, count.keys(), key=count.get)
 
+if __name__ == '__main__':
+
+    lt = [1,1,1,2,2,3]
+    k = 2
+    print(Solution().topKFrequent(lt,k))
